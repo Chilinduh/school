17696f8b74cda9a25219dc3dfad85dba7677351b
Chilinduh <kurt_dc@mail.ru>
2024-10-13 21:39:33 +0300

    sdfdsf

--- frontend/web/files/custom.js
+++ frontend/web/files/custom.js
@@ -13,6 +13,7 @@ function sendCallBack() {
             $('.wpcf7-response-output').css('visibility', 'visible');
             $('#ohio-custom-67005dfe5c410').find('button').removeClass('btn-loading');
             $('.wpcf7-response-output').find('h5').html(data.message)
+            $('#CallBack-text').val('');
         },
         error: function (response) {
 
