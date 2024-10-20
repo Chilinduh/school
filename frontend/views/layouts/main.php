c28de7d7e505db3b50b7b32cab6c44998e74ade1
Chilinduh <kurt_dc@mail.ru>
2024-10-20 16:10:01 +0300

    fix

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -705,17 +705,7 @@ AppAsset::register($this);
             }
         }
     </style>
-    <meta name="generator" content="Powered by WPBakery Page Builder - drag and drop page builder for WordPress.">
-    <link rel="icon"
-          href="/files/Opz4s8nI-cropped-favicon-32x32.png"
-          sizes="32x32">
-    <link rel="icon"
-          href="/files/Opz4s8nI-cropped-favicon-192x192.png"
-          sizes="192x192">
-    <link rel="apple-touch-icon"
-          href="/files/Opz4s8nI-cropped-favicon-180x180.png">
-    <meta name="msapplication-TileImage"
-          content="/files/Opz4s8nI-cropped-favicon-270x270.png">
+
     <style id="sccss">
         /*Promo banner --- starts*/
         .promo-banner {
